37d540e2673d16acd61d07146f8b9cd08a0c18cc
gavs9 <gavs272@gmail.com>
2016-11-01 00:11:00 -0600

    route model

--- models/route.model.js
+++ models/route.model.js
@@ -5,7 +5,21 @@
 var mongoose = require('mongoose');
 
 var routeSchema = mongoose.Schema({ //Missing data schema
-	name:{type:String,require: true},
+	nombre:{type:String,require: true},
+	rutas:[{
+		nombre:{typre:String},
+		horarios:[{
+			inicio:{type:Number},
+			final:{type:Number},
+			hora:{type:Number},
+			paradas:[{
+				proveniente:{type:Number},
+				hora1:{type:Number},
+				destino:{type:Number},
+				hora2:{type:Number}
+			}]
+		}]
+	}]
 },{collection:'route'});
 
 var Route = mongoose.model('Route',routeSchema);
